b8b64c7d81884019878b72839ffe280d56795825
Ayeika-tech <68619379+Ayeika-tech@users.noreply.github.com>
2021-12-11 20:18:55 -0600

    Restructured solution

---
+++ Demo.DS.BinaryTree.Lib/AVLTree/AVLTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.DS.BinaryTree.Lib.BinaryTree;
+
+namespace Demo.DS.BinaryTree.Lib.AVLTree
+{
+    public class AVLTree : BinaryTree.BinaryTree
+    {
+
+    }
+}
---
+++ Demo.DS.BinaryTree.Lib/AVLTree/AVLTreeNode.cs
@@ -0,0 +1,16 @@
+﻿using Demo.DS.BinaryTree.Lib.BinaryTree;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DS.BinaryTree.Lib.AVLTree
+{
+    public class AVLTreeNode : Node
+    {
+        public AVLTreeNode(int data) : base(data)
+        {
+        }
+    }
+}
